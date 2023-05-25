1de7182dce9653b1e6a00c4a637ddd00d9c7a740
Durga Mahesh <105915618+Mahesh7202@users.noreply.github.com>
2023-05-25 15:40:33 +0530

    old

--- src/main/java/org/example/DoReMiSubscriptions/DoReMiSubscriptions.java
+++
@@ -1,9 +0,0 @@
-package org.example.DoReMiSubscriptions;
-
-import org.example.DoReMiSubscriptions.controller.SubscriptionManager;
-
-public class DoReMiSubscriptions {
-    public static void main(String[] args) {
-            SubscriptionManager.start("D://DoReMi-Subscriptions//sample_input//input2.txt");
-    }
-}
--- src/main/java/org/example/DoReMiSubscriptions/constants/SubscriptionCategory.java
+++
@@ -1,7 +0,0 @@
-package org.example.DoReMiSubscriptions.constants;
-
-public enum SubscriptionCategory {
-    MUSIC,
-    VIDEO,
-    PODCAST
-}
